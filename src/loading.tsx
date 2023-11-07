5140e8416243c61055e503a91fb83b450db5da1f
xu756 <756334744@qq.com>
2023-11-08 00:25:00 +0800

    init

---
+++ src/loading.tsx
@@ -0,0 +1,5 @@
+const LoadingPage = () => {
+    return <>11</>;
+};
+
+export default LoadingPage;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+import './index.scss';
+
+const HomePage = () => {
+    return <>11</>;
+};
+
+export default HomePage;
